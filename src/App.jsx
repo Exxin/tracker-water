1ae2bf32c2598c9dc6cd901d36eebb3fd9aaa2b5
Exxin <exxinblood@gmail.com>
2024-08-29 12:01:26 +0300

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import { useDispatch, useSelector } from 'react-redux';
 // import { selectIsLoggedIn } from '../redux/auth/selectors';
 import 'modern-normalize';
 import { Route, Routes } from 'react-router-dom';
-import SharedLayout from './components/SharedLayout';
-import RestrictedRoute from './components/RestrictedRoute';
-import PrivateRoute from './components/PrivateRoute';
+// import SharedLayout from './components/SharedLayout';
+// import RestrictedRoute from './components/RestrictedRoute';
+// import PrivateRoute from './components/PrivateRoute';
 
 import 'modern-normalize';
 import { selectIsLoggedIn, selectIsRefreshing } from './redux/auth/selectors';
